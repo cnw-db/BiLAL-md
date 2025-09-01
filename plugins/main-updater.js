b950c86ff6bfb2a192066e06d2b95bc32f5c52c0
cnw-db <newbot1952@gmail.com>
2025-09-01 21:47:45 +0530

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,10 +16,10 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for BILAL-MD updates...");
+        await reply("🔍 Checking for BiLAL-MD updates...");
 
-        // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/chamod-os/Whiteshadow-v5.0/commits/main");
+        // Fetch the latest commit hash from GitHub (BiLAL-md repo)
+        const { data: commitData } = await axios.get("https://api.github.com/repos/cnw-db/BiLAL-md/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
@@ -29,11 +29,11 @@ cmd({
             return reply("✅ Your bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating BILAL-MD Bot...");
+        await reply("🚀 Updating BiLAL-MD Bot...");
 
-        // Download the latest code
+        // Download the latest code (BiLAL-md repo)
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/chamod-os/Whiteshadow-v5.0/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/cnw-db/BiLAL-md/archive/refs/heads/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "Whiteshadow-v5.0-main");
+        const sourcePath = path.join(extractPath, "BiLAL-md-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
